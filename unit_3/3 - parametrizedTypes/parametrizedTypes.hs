4ea2c4143fc1adcfe1a3a32591af0b711e56ab1c
QLR <quentin.leroux@edhec.com>
2020-08-14 23:05:34 +0200

    13th commit

--- unit_3/3 - parametrizedTypes/parametrizedTypes.hs
+++ unit_3/3 - parametrizedTypes/parametrizedTypes.hs
@@ -1,4 +1,53 @@
+data Box a = Box a deriving Show
+data Triple a = Triple a a a deriving Show
 
+wrap :: a -> Box a
+wrap x = Box x
+
+unwrap :: Box a -> a
+unwrap (Box x) = x
+
+--
+
+type Point3D = Triple Double
+aPoint :: Point3D
+aPoint = Triple 0.1 53.2 12.3
+
+type FullName = Triple String
+aPerson :: FullName
+aPerson = Triple "Howard" "Philips" "Lovecraft"
+
+type Initials = Triple Char
+initials :: Initials
+initials = Triple 'H' 'P' 'L'
+
+--
+
+first :: Triple a -> a
+first (Triple x _ _) = x
+
+second :: Triple a -> a
+second (Triple _ x _) = x
+
+third :: Triple a -> a
+third (Triple _ _ x) = x
+
+toList :: Triple a -> [a]
+toList (Triple x y z) = [x, y, z]
+
+transform :: (a -> a) -> Triple a -> Triple a
+transform f (Triple x y z) = Triple (f x) (f y) (f z)
 
 main = do
-  print()
+  let x = 6::Int
+  let y = wrap x 
+  print(y)
+  print(unwrap y)
+  --
+  print(aPoint)
+  print(first aPoint)
+  print(second aPerson)
+  print(third initials)
+  print(toList aPerson)
+  print(transform (*3) aPoint)
+  print(transform reverse aPerson)
---
+++ unit_3/4 - maybe/maybe2.hs
@@ -0,0 +1,59 @@
+import qualified Data.Map as Map
+import Data.List
+
+data Organ = Heart | Brain | Kidney | Spleen deriving (Show,  Eq)
+--data Maybe a = Nothing | Just a
+
+organs :: [Organ]
+organs = [Heart, Heart, Brain, Spleen, Spleen, Kidney]
+
+ids :: [Int]
+ids = [2,7,13,14,21,24]
+
+organPairs :: [(Int, Organ)]
+organPairs = zip ids organs
+
+organCatalog :: Map.Map Int Organ
+organCatalog = Map.fromList organPairs
+
+data Container = Vat Organ | Cooler Organ | Bag Organ
+instance Show Container where
+  show (Vat organ) = show organ ++ " in a vat"
+  show (Cooler organ) = show organ ++ " in a cooler"
+  show (Bag organ) = show organ ++ " in a bag"
+
+data Location = Lab | Kitchen | Bathroom deriving Show
+
+organToContainer :: Organ -> Container
+organToContainer Brain = Vat Brain
+organToContainer Heart = Cooler Heart
+organToContainer organ = Bag organ
+
+placeInLocation :: Container -> (Location, Container)
+placeInLocation (Vat a) = (Lab, Vat a)
+placeInLocation (Cooler a) = (Lab, Cooler a)
+placeInLocation (Bag a) = (Kitchen, Bag a)
+
+process :: Organ -> (Location, Container)
+process organ = placeInLocation (organToContainer organ)
+
+report :: (Location, Container) -> String
+report (location, container) = show container ++ " in the " ++ show location
+
+processAndReport :: (Maybe Organ) -> String
+processAndReport (Just organ) = report (process organ)
+processAndReport Nothing = "error, id not found"
+
+processRequest :: Int -> Map.Map Int Organ -> String
+processRequest id catalog = processAndReport organ
+  where organ = Map.lookup id catalog
+  
+main = do
+  print(process Brain)
+  print(process Heart)
+  print(process Spleen)
+  print(process Kidney)
+  print(report (process Brain))
+  print(report (process Spleen))
+  print(processRequest 13 organCatalog)
+  print(processRequest 12 organCatalog)
